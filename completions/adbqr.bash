17fa2c0bee8b9c6d91aa8fa3f2ac42c7c1bbaa10
Nefla <neflaprojekt@gmail.com>
2026-08-27 13:42:26 +0900

    fix(ci): address shellcheck lint warnings in scripts and completions

--- completions/adbqr.bash
+++ completions/adbqr.bash
@@ -10,15 +10,16 @@ _adbqr_completions() {
 
     case "$prev" in
         -t|--timeout)
-            COMPREPLY=( $(compgen -W "10 30 60 120" -- "$cur") )
+            mapfile -t COMPREPLY < <(compgen -W "10 30 60 120" -- "$cur")
             return 0
             ;;
     esac
 
     if [[ "$cur" == -* ]]; then
-        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
         return 0
     fi
 }
 
 complete -F _adbqr_completions adbqr
+
